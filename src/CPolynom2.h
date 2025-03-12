f7fd03e31c5328139d524f0aafeafd041ef879ee
taras <luviq7@gmail.com>
2025-03-12 17:37:09 +0200

    final_build_without_zvit

--- src/CPolynom2.h
+++ src/CPolynom2.h
@@ -8,30 +8,24 @@ private:
     double c;
 
 public:
-    // Конструктори
     CPolynom2();
     CPolynom2(double coefA, double coefC);
     CPolynom2(const CPolynom2& other);
 
-    // Гетери
     double getA() const;
     double getC() const;
 
-    // Сетери
     void setA(double coefA);
     void setC(double coefC);
 
-    // Функції-члени
     double evaluate(double x) const;
     double derivative() const;
     double findRoot() const;
 
-    // Оператори
     CPolynom2 operator+(const CPolynom2& other) const;
     CPolynom2 operator-(const CPolynom2& other) const;
     CPolynom2 operator*(double scalar) const;
 
-    // Додаткові методи
     void negate();
     void print() const;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,30 @@
 using namespace std;
 
 int main() {
-    // Створення об'єктів
     CPolynom2 p1(3, 5);
     CPolynom2 p2(2, -1);
 
-    // Вивід поліномів
     cout << "p1: ";
     p1.print();
-    
     cout << "p2: ";
     p2.print();
-
-    // Додавання поліномів
     CPolynom2 sum = p1 + p2;
     cout << "Сума: ";
     sum.print();
 
-    // Віднімання поліномів
     CPolynom2 diff = p1 - p2;
     cout << "Різниця: ";
     diff.print();
 
-    // Множення на скаляр
     CPolynom2 scaled = p1 * 2;
     cout << "Множення p1 на 2: ";
     scaled.print();
 
-    // Обчислення значення полінома при x = 2
     double value = p1.evaluate(2);
     cout << "Значення p1(2): " << value << endl;
 
-    // Знаходження похідної
     cout << "Похідна p1: " << p1.derivative() << endl;
 
-    // Знаходження кореня рівняння
     try {
         double root = p1.findRoot();
         cout << "Корінь p1: " << root << endl;
@@ -45,7 +35,6 @@ int main() {
         cout << "Помилка: " << e.what() << endl;
     }
 
-    // Зміна знаку коефіцієнтів p1
     p1.negate();
     cout << "Змінений знак p1: ";
     p1.print();
