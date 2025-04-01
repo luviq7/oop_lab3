972e8481fe3ec5022ce3f92106616ab636899931
taras <luviq7@gmail.com>
2025-04-01 22:56:37 +0000

    CMAKE

---
+++ build/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/src/CPolynom2.cpp.o"
+  "CMakeFiles/runTests.dir/src/CPolynom2.cpp.o.d"
+  "CMakeFiles/runTests.dir/tests/tests.cpp.o"
+  "CMakeFiles/runTests.dir/tests/tests.cpp.o.d"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
